19e3bdc99e6559c4d2679aa70169c2f6bf217573
Soria Alvaro <81036909+mariafvilera@users.noreply.github.com>
2022-08-26 19:00:27 -0300

    Miniproblema 1 finalizado

---
+++ Problema1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Problema1;
+
+public class Program{
+    public static void Main(string[] args){
+        try{
+            int entrada = Console.ReadLine();
+            Console.WriteLine(entrada * entrada);
+        }
+        catch(Exception ex){
+            Console.WriteLine(ex);
+        }
+    }
+}
