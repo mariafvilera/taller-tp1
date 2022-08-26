ad3003c92bd83b5481c01ea25d8a4d68d9300a72
Soria Alvaro <81036909+mariafvilera@users.noreply.github.com>
2022-08-26 19:14:56 -0300

    Miniproblema 3 finalizado

---
+++ Problema3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Problema3;
+
+public class Program{
+
+    public static void Main(string[] args){
+        Console.WriteLine("Ingrese la cantidad de kilometros conducidos");
+        float kilometros = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingre la cantidad de litros usados");
+        float litros = Convert.ToInt32(Console.ReadLine());
+        
+        float kilometrosPorLitro;
+
+        try{
+            kilometrosPorLitro = kilometros / litros;
+        }
+        catch(Exception ex){
+            Console.WriteLine(ex);
+        }
+        finally{
+            Console.WriteLine("Kilometros por litro: " + kilometrosPorLitro);
+        }
+    }
+}
