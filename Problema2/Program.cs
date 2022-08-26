041396f596b4226f52ec7626d551e01b50326b5c
Soria Alvaro <81036909+mariafvilera@users.noreply.github.com>
2022-08-26 19:01:47 -0300

    Miniproblema 2 finalizado

---
+++ Problema2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Ingrese el primer número");
+int numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo número");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+
+try{
+    int division = numero1 / numero2;
+}
+catch(Exception ex){
+    Console.WriteLine(ex);
+}
+
+Console.WriteLine("Resultado " + division);
